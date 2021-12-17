48bc89e3616ee558bf4b93d72d34edca1ea0b73b
carloo07 <76703454+carloo07@users.noreply.github.com>
2021-12-17 17:29:26 +0300

    Add files via upload

--- src/slashCommands/Music/247.js
+++ src/slashCommands/Music/247.js
@@ -1,1 +1,38 @@
+const { MessageEmbed, CommandInteraction, Client } = require("discord.js")
 
+module.exports = {
+  name: "247",
+  description: "24/7 in voice channel",
+
+  run: async (client, interaction) => {
+    await interaction.deferReply({
+      ephemeral: false
+    });
+    if (!interaction.member.voice.channel) return interaction.editReply({ embeds: [new MessageEmbed().setColor(client.embedColor).setDescription("You are not connect in vc")] });
+    if (interaction.guild.me.voice.channel && interaction.guild.me.voice.channelId !== interaction.member.voice.channelId) return interaction.editReply({ embeds: [new MessageEmbed().setColor(client.embedColor).setDescription(`You are not connected to <#${interaction.guild.me.voice.channelId}> to use this command.`)] });
+
+    const player = interaction.client.manager.get(interaction.guildId);
+    if (!player) {
+      const embed = new MessageEmbed()
+        .setDescription('there is nothing playing')
+        .setColor(client.embedColor)
+      return interaction.editReply({ embeds: [embed] });
+    }
+    const { channel } = interaction.member.voice
+
+    if (player.twentyFourSeven) {
+      player.twentyFourSeven = false;
+      const embed = new MessageEmbed()
+        .setDescription("24/7 mode is **Disabled**")
+        .setColor(client.embedColor)
+      return interaction.editReply({ embeds: [embed] });
+    } else {
+      player.twentyFourSeven = true;
+      const embed = new MessageEmbed()
+        .setDescription("24/7 mode is **Enabled**")
+        .setColor(client.embedColor)
+      return interaction.editReply({ embeds: [embed] });
+
+    }
+  }
+}
