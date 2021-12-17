a99ed329d8c39fa032492bcdedcecfd7848e00e6
carloo07 <76703454+carloo07@users.noreply.github.com>
2021-12-17 17:11:58 +0300

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,1 +1,22 @@
+module.exports = {
+    token: "",
+    prefix: "",
+    ownerID: "",
+    SpotifyID: "",
+    SpotifySecret: "",
+    mongourl: "",
+    embedColor: "",
+    logs: "",
 
+  nodes: {
+     
+      host: "disbotlistlavalink.ml",
+      port: 443,
+      password: "LAVA",
+      id: "DisBotlist Lavalink",
+      retryDelay: 3000,
+      secure: true
+    
+    },
+ 
+}
