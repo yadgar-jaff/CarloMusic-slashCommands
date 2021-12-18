a20e28ff7b0fa6a258a8174c8c8df8ae92fd9e32
carloo07 <76703454+carloo07@users.noreply.github.com>
2021-12-18 06:15:26 +0300

    Update ready.js

--- src/events/Client/ready.js
+++ src/events/Client/ready.js
@@ -5,11 +5,10 @@ module.exports = async (client) => {
     client.logger.log(`${client.user.username} online!`, "ready");
     client.logger.log(`Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`, "ready");
     //Game
-    let statuses = ['/help', `${prefix}help`];
+    let statuses = ['/help - ${client.guilds.cache.size} Guilds', `${prefix}help Created by Carlo`];
     setInterval(function() {
   		let status = statuses[Math.floor(Math.random()*statuses.length)];
   		client.user.setActivity(status, {type: "PLAYING"});
-                client.user.setStatus("idle");
   	}, 10000)
 
 }
