4336e7d6ee207422747af7503e2eec60a1fc23fe
yadgar-jaff <yada.h.rasul1@gmail.com>
2024-05-09 17:55:34 +0300

    Update prefix.js

--- src/schema/prefix.js
+++ src/schema/prefix.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const productSchema = mongoose.Schema({
   _id: {type: mongoose.Schema.Types.ObjectId, required: true},
   guildid: {type:String, required:true},
-  prefix: {type:String, required:true, default: ""}
+  prefix: {type:String, required:true, default: "-"}
 });
 
 module.exports = mongoose.model("prefix", productSchema);
